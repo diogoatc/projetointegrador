0ddab071efa3e1a8e0f976957695c37253c5295d
Diogo Lopes <diogol_l@hotmail.com>
2015-10-25 17:50:39 -0200

    enviado duas classes

--- tela1.php
+++ tela1.php
@@ -1,5 +1,36 @@
 <?php
 
+class aluno{
+	public $ra;
+	public $nome;
+	public $turma;
+	public $externo;
+	
+	public $host   ='localhost';
+	public $user   ='root';
+	public $pw     ='';
+	public $db     ='alunos';
+	
+		function pesquisara($ra){
+			$this->conecta_bd();
+			
+			$this->ra =$ra;
+			
+		}
+	
+	
+}
+
+
+
+
+
+class alunos{
+	private $nome;
+	private $ra;
+	private $turma;
+	private $externo;
+
  function pesquisaaluno(){
 	
  	
@@ -16,21 +47,27 @@
  	$query = $mysqli->query($sql);
  	
  	while ($dados = $query->fetch_array()) {
- 		echo $dados['nome']."<br/>";
- 		echo $dados['ra']."<br/>";
- 		echo $dados['externo']."<br/>";
+ 		$dadosnome=$dados['nome'];
+ 		$dadosra=$dados['ra'];
+ 		$dadosexterno=$dados['externo'];
+ 		$dadoscurso=$dados['curso'];
  	}
+ 		return $dadosnome;
+ 		return $dadosra;
+ 		return $dadosexterno;
+ 		return $dadosturma;
+ 		echo $dadosnome;
  	mysqli_close($mysqli);
 }
-if(isset($_POST['pesquisa'])){
-pesquisaaluno();
 }
 
+if (isset ( $_POST ['pesquisa'] )) {
+	alunos.pesquisaaluno ();
+}
 ?>
 
 <form action="" name="pesquisa" method="post">
-	<input type="text" name="ra">
-	<input type="submit" name="pesquisa">
+	<input type="text" name="ra"> <input type="submit" name="pesquisa">
 
 
 </form>
--- tela2.php
+++ tela2.php
@@ -1,6 +1,6 @@
 <?php
 include 'tela1.php';
 
-mostraalunos($query);
+echo $dadosnome;
 
 ?>
