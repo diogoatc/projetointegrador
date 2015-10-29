4d73b08c0ca65768b641dc23fe756d364d7f24f2
Diogo Lopes <diogol_l@hotmail.com>
2015-10-29 16:31:21 -0200

    mudancas

--- Conecta.php
+++ Conecta.php
@@ -1,50 +1,37 @@
 <?php
 include 'tela1.php';
-	class Conecta{
-	 public  $conexao;
-		public function conectar(){
-		
-		$servidor= 'localhost';
+class Conecta {
+	public $conexao;
+	public function conectar() {
+		$servidor = 'localhost';
 		$bd = 'alunos';
-		$senha='';
-		$usuario='root';
+		$senha = '';
+		$usuario = 'root';
 		
-		$this->conexao= new PDO("mysql:host=$servidor;dbname=$bd", $usuario, "$senha");
-		
-}
+		$this->conexao = new PDO ( "mysql:host=$servidor;dbname=$bd", $usuario, "$senha" );
 	}
-class listar extends Conecta{
-	
-	public  function lista(){
-		$ra=$_POST['ra'];
-		$this->conectar();
-		$rs= $this->conexao->query("SELECT * FROM aluno WHERE ra=$ra");
-		$a=new Aluno();
-		while ($row= $rs->fetch(PDO::FETCH_OBJ)){
+}
+class listar extends Conecta {
+	public function lista() {
+		$ra = $_POST ['ra'];
+		$this->conectar ();
+		$rs = $this->conexao->query ( "SELECT * FROM aluno WHERE ra=$ra" );
+		$a = new Aluno ();
+		while ( $row = $rs->fetch ( PDO::FETCH_OBJ ) ) {
 			
-			$a->ra=$row->ra;
-			$a->nome=$row->nome;
-			$a->interno=$row->interno;
-			$a->curso=$row->curso;
-			
+			$a->ra = $row->ra;
+			$a->nome = $row->nome;
+			$a->interno = $row->interno;
+			$a->curso = $row->curso;
 		}
 		return $a;
-	} 
-	
-	
-
-
+	}
 }
- class Aluno{
- 	public $ra;
- 	public $nome;
- 	public $curso;
- 	public $interno;
- 	
- }
-		
-
-	
-
+class Aluno {
+	public $ra;
+	public $nome;
+	public $curso;
+	public $interno;
+}
 
 ?>
--- tela2.php
+++ tela2.php
@@ -1,30 +1,30 @@
-<?php 
-if (isset($_POST['envia']))
-?>
 <?php
-include 'Conecta.php';
-$x=new listar();
-$aluno=$x->lista();
+if (isset ( $_POST ['envia'] ))
+	?>
+<?php
 
+include 'Conecta.php';
+$x = new listar ();
+$aluno = $x->lista ();
 
 ?>
 <table width='200' border='1' cellpadding='1'>
-  <tr>
-  	<th>RA </th>
-    <th><?=$aluno->ra?></th>
-  </tr>
-  <tr>
-  	<th>Nome</th>
-    <th><?=$aluno->nome?></th>
-  </tr>
-  <tr>
-  	<th>Curso</th>
-    <th><?=$aluno->curso?></th>
-  </tr>
-  <tr>
-  	<th>Regime</th>
-    <th><?=$aluno->interno?></th>
-  </tr>
+	<tr>
+		<th>RA</th>
+		<th><?=$aluno->ra?></th>
+	</tr>
+	<tr>
+		<th>Nome</th>
+		<th><?=$aluno->nome?></th>
+	</tr>
+	<tr>
+		<th>Curso</th>
+		<th><?=$aluno->curso?></th>
+	</tr>
+	<tr>
+		<th>Regime</th>
+		<th><?=$aluno->interno?></th>
+	</tr>
 </table>
 
 
