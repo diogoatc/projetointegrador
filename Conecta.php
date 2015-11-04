b9bacaeca69622f7dffe9eb8cbaa98b81e3c49b5
Diogo Lopes <diogol_l@hotmail.com>
2015-11-04 21:48:47 -0200

    alteracao da tela 3, alteracao da Conecta.php

--- Conecta.php
+++ Conecta.php
@@ -44,7 +44,48 @@ class listar extends Conecta {
  	
  }
 		
-
+ class emprestados extends Conecta{
+ 	public function listatudo(){
+ 		$this->conectar();
+ 		$rs=$this->conexao->query("SELECT relac_aluno_material.ordemservico, relac_aluno_material.ra_aluno, materiais.materiais FROM materiais INNER JOIN relac_aluno_material ON relac_aluno_material.id_material=materiais.id");
+ 		$b= new Material();
+ 		while ($row = $rs->fetch (PDO::FETCH_OBJ)){
+ 			
+ 			
+ 		echo "<table width='200' border='1' cellspacing='3' cellpadding='2'>
+  				<tr>
+    				<th scope='row'>$row->ordemservico</th>
+    				<td>$row->ra_aluno</td>
+    				<td>$row->materiais</td>
+  				</tr>
+				</table>";
+ 			
+ 			
+ 			
+ 			
+ 			
+ 			
+ 			
+ 			$b->raaluno= $row->ra_aluno;
+ 			$b->ordemservico=$row->ordemservico;
+ 			$b->materiais=$row->materiais;
+ 			
+ 
+ 		}
+ 		
+ 		echo "<a href='tela1.php'>Home></a>";
+ 		return $b;
+ 	}
+ 
+ 
+ }
+ 
+ class Material{
+ 	public $raaluno;
+ 	public $ordemservico;
+ 	public $materiais;
+ 
+ }
 	
 
 
--- tela3.php
+++ tela3.php
@@ -1,6 +1,10 @@
 <?php
+include "Conecta.php";
 
+$x= new emprestados();
+$lista=$x->listatudo();
 
-
-
+echo $lista;
+echo "<a href='tela1.php'>Home></a>";
 ?>
+
